4feca8aa7a8d568acb39706e456246e50f2e41de
Tongueta14 <german.gaston.rodriguez@gmail.com>
2020-05-12 22:39:25 -0300

    Initial Commit

---
+++ src/main/java/com/uade/financialGame/FinancialGameApplication.java
@@ -0,0 +1,13 @@
+package com.uade.financialGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/financialGame/FinancialGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.financialGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
