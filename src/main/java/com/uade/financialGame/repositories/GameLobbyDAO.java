755449b9b4e50f91df0d699890fec97349a6421c
ebdk <edbedecarats@gmail.com>
2020-05-31 14:19:15 -0300

    Removed GameLobbyDao

--- src/main/java/com/uade/financialGame/repositories/GameLobbyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.uade.financialGame.repositories;
-
-import com.uade.financialGame.models.GameLobby;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GameLobbyDAO extends JpaRepository<GameLobby, Long> {
-}
